0f1b5ec7f015103b358a734fe23df6154838cad8
unknown <hasanbaratiasp@gmail.com>
2021-06-12 22:28:01 +0430

    card change style

--- src/page/shop/shop.js
+++ src/page/shop/shop.js
@@ -1,22 +1,88 @@
 import React from 'react'
-
+import './shop.style.css'
 const Shop=()=>{
     return(
         <div className="row mb-4">
-            <div className="col-md-10 mb-4">
-                <div className="cardsection col-md-4 mb-4">
-                    <div className="view zoom z-depth-2 rounded">
-                    <img src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
+            <div className="col-md-9 mb-4">
+                <div className="row m-4">
+                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                        <div className="view zoom z-depth-2 rounded">
+                        <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
+
+                    </div>
+                    <div className="info p-3">
+                        <div className="prices d-flex justify-content-between">
+                           <span className="d-flex justify-content-end">lemon</span>
+                            <span className="d-flex justify-content-end">155 </span>
+                            
+                        </div>
+
+                        <button className="buy btn btn-light mt-2"><span className="decrease d-none">-</span>   افزودن به سبد خرید   <span className="increase">+</span></button>
+                    </div>
+                          
+                    </div> 
+
+
+                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                        <div className="view zoom z-depth-2 rounded">
+                        <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
+
+                    </div>
+                    <div className="info p-3">
+                        <div className="prices d-flex justify-content-between">
+                           <span className="d-flex justify-content-end">lemon</span>
+                            <span className="d-flex justify-content-end">155 </span>
+                            
+                        </div>
+
+                        <button className="buy btn btn-light mt-2"><span className="decrease d-none">-</span>   افزودن به سبد خرید   <span className="increase">+</span></button>
+                    </div>
+                          
+                    </div> 
+
+
+                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                        <div className="view zoom z-depth-2 rounded">
+                        <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
+
+                    </div>
+                    <div className="info p-3">
+                        <div className="prices d-flex justify-content-between">
+                           <span className="d-flex justify-content-end">lemon</span>
+                            <span className="d-flex justify-content-end">155 </span>
+                            
+                        </div>
+
+                        <button className="buy btn btn-light mt-2"><span className="decrease d-none">-</span>   افزودن به سبد خرید   <span className="increase">+</span></button>
+                    </div>
+                          
+                    </div> 
+
+
+                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                        <div className="view zoom z-depth-2 rounded">
+                        <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
+
+                    </div>
+                    <div className="info p-3">
+                        <div className="prices d-flex justify-content-between">
+                           <span className="d-flex justify-content-end">lemon</span>
+                            <span className="d-flex justify-content-end">155 </span>
+                            
+                        </div>
+
+                        <button className="buy btn btn-light mt-2"><span className="decrease d-none">-</span>   افزودن به سبد خرید   <span className="increase">+</span></button>
+                    </div>
+                          
+                    </div> 
+
+                    
 
                 </div>
-                <div className="info">
-                    <span className="float-end">155 </span>
-                    <span className="float-end">lemon</span>
-                </div>
-
-                </div> 
+                
+                
             </div>
-            <div className="col-md-2 mb-4">
+            <div className="col-md-3 mb-4">
 
             </div>
         </div>
