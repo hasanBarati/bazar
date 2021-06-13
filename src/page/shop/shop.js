749c69e55bf46886c708a01e324d77a04946f156
unknown <hasanbaratiasp@gmail.com>
2021-06-13 17:52:20 +0430

    sidebar created

--- src/page/shop/shop.js
+++ src/page/shop/shop.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './shop.style.css'
+import { Link,Route } from 'react-router-dom'
+import Fruit from '../../assests/svg/fruit.svg'
+
 const Shop=()=>{
     return(
         <div className="row mb-4">
             <div className="col-md-9 mb-4">
                 <div className="row m-4">
-                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                    <div className="cardsection col-md-3   p-0 bg-white ">
                         <div className="view zoom z-depth-2 rounded">
                         <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
 
@@ -23,7 +26,7 @@ const Shop=()=>{
                     </div> 
 
 
-                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                    <div className="cardsection col-md-3   p-0 bg-white ">
                         <div className="view zoom z-depth-2 rounded">
                         <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
 
@@ -41,7 +44,7 @@ const Shop=()=>{
                     </div> 
 
 
-                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                    <div className="cardsection col-md-3   p-0 bg-white ">
                         <div className="view zoom z-depth-2 rounded">
                         <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
 
@@ -59,7 +62,7 @@ const Shop=()=>{
                     </div> 
 
 
-                    <div className="cardsection col-md-2 m-1  p-0 bg-white ">
+                    <div className="cardsection col-md-3   p-0 bg-white ">
                         <div className="view zoom z-depth-2 rounded">
                         <img className="w-100" src="https://res.cloudinary.com/redq-inc/image/upload/c_fit,q_auto:best,w_300/v1589736058/pickbazar/grocery/codfillet_u0mti1.jpg" />
 
@@ -83,7 +86,30 @@ const Shop=()=>{
                 
             </div>
             <div className="col-md-3 mb-4">
-
+                <div className="row m-4">
+               
+                    <div className="col-md-5 bg-white m-1 p-3">
+                    <Link to="">
+                            
+                        
+                        <div className="icon">
+                        <img src={Fruit} width="100" height="50" /> 
+                         
+                        </div>
+                        <p className="text-center mt-2">Fruit</p>
+                    </Link>  
+                    </div>
+                  
+                    <div className="col-md-5 bg-white m-1 p-3">
+                        <div className="icon">
+                        <img src={Fruit} width="100" height="50" /> 
+                         
+                        </div>
+                        <p className="text-center mt-2">Fruit</p>
+                    </div>
+                    
+                </div>
+                    
             </div>
         </div>
     )
